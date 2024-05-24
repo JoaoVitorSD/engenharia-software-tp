cf7524e1fe7909de4f048601114ece5dce03f485
pedro-de-oliveira-guedes <pedro.og2002@gmail.com>
2024-05-24 18:39:54 -0300

    Ref: Extracting genReport functionalities to methods.

--- src/main/java/tp/gerenciamento/infraestructure/adapter/RelatorioGatewayAdapter.java
+++ src/main/java/tp/gerenciamento/infraestructure/adapter/RelatorioGatewayAdapter.java
@@ -17,28 +17,38 @@ public class RelatorioGatewayAdapter implements RelatorioGateway {
 
     @Override
     public LinkedList<HashMap<String, Object>> genReport() {
+        LinkedList<HashMap<String, Object>> report = new LinkedList<>();
+        report.add(getEnderecosGroupByEstado());
+        report.add(getEnderecosGroupedByEstadoAndCidade());
+
+        return report;
+    }
+
+    private HashMap<String, Object> getEnderecosGroupByEstado() {
         HashMap<String, Object> enderecosEstados = new HashMap<>();
-        List<Map<String,Object>> result =  jdbcTemplate.queryForList("SELECT e.estado, count(e.estado) as ocorrencias FROM endereco e group by e.estado order by ocorrencias desc");
-        for(Map<String,Object> row : result){
-            enderecosEstados.put((String)row.get("estado"), row.get("ocorrencias"));
+        List<Map<String, Object>> result = jdbcTemplate.queryForList("SELECT e.estado, count(e.estado) as ocorrencias " +
+                "FROM endereco e group by e.estado order by ocorrencias desc");
+        for (Map<String, Object> row : result) {
+            enderecosEstados.put((String) row.get("estado"),  row.get("ocorrencias"));
         }
-        result = jdbcTemplate.queryForList("SELECT e.estado, e.cidade, count(e.cidade) as ocorrencias FROM endereco e group by e.estado, e.cidade order by ocorrencias desc");
+        return enderecosEstados;
+    }
+
+    private HashMap<String, Object> getEnderecosGroupedByEstadoAndCidade() {
+        List<Map<String, Object>> result = jdbcTemplate.queryForList("SELECT e.estado, e.cidade, count(e.cidade) as ocorrencias " +
+                "FROM endereco e group by e.estado, e.cidade order by ocorrencias desc");
 
         HashMap<String, Object> cidadesEstados = new HashMap<>();
-        for(Map<String,Object> row : result){
-            if(cidadesEstados.containsKey(row.get("estado"))){
-                ((HashMap<String, Object>)cidadesEstados.get(row.get("estado")))
-                        .put((String)row.get("cidade"), row.get("ocorrencias"));
-            }else{
+        for (Map<String, Object> row : result) {
+            if (cidadesEstados.containsKey(row.get("estado"))) {
+                ((HashMap) cidadesEstados.get(row.get("estado")))
+                        .put( row.get("cidade"),row.get("ocorrencias"));
+            } else {
                 HashMap<String, Object> cidade = new HashMap<>();
-                cidade.put((String)row.get("cidade"), row.get("ocorrencias"));
-                cidadesEstados.put((String)row.get("estado"), cidade);
+                cidade.put((String) row.get("cidade"),  row.get("ocorrencias"));
+                cidadesEstados.put((String) row.get("estado"), cidade);
             }
         }
-        LinkedList<HashMap<String, Object>> resultado = new LinkedList<>();
-        resultado.add(enderecosEstados);
-        resultado.add(cidadesEstados);
-
-        return resultado;
+        return cidadesEstados;
     }
 }
